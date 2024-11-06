2b14ec12b98b29ff4ef863057dff44252f532c3e
vinnietito <vincentnjuguna69@gmail.com>
2024-11-06 10:38:58 +0300

    Commits

---
+++ SlicingList.py
@@ -0,0 +1,8 @@
+languages = ["Python", "Swift", "C++"]
+
+print (languages[-1])
+#List slicing in python
+my_list = ['p', 'r', 'o', 'g', 'r', 'a', 'm', 'i']
+
+#Items from index 2 to index 4
+print(my_list[2:5]) #Output: 
