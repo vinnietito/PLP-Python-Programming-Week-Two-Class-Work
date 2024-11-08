7b6125dd792dad352e2233e07d885c8207011697
vinnietito <vincentnjuguna69@gmail.com>
2024-11-08 10:21:33 +0300

    Commits

--- dict.py
+++ dict.py
@@ -7,4 +7,4 @@ students = {
 }
 
 
-print(students[12])
+print(students["Name"])
---
+++ list_practical.py
@@ -0,0 +1,10 @@
+list_a = [1, 2, 3, 4, 5]
+print(list_a[2])
+
+list_b = [1, "today", True, 3.0]
+
+list_c = ["Skinny", "Ahmad", 'Mutemi']
+
+list_d = [1, 2, 3, [4, 5], 6, 7]
+
+list_e = []
