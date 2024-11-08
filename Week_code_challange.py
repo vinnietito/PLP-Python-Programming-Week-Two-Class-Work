6c6bcc431330f7dc47870a7d1fe20faf18f55f82
vinnietito <vincentnjuguna69@gmail.com>
2024-11-08 11:13:02 +0300

    Commits

--- Week_code_challange.py
+++ Week_code_challange.py
@@ -1,7 +1,18 @@
+# 1. Program to Create a List of Integers and Compute Their Sum
 #Accepting user input to create a list of integers
-numbers = input("Enter integers separated by spaces: ")
-int_list = list(map(int, numbers.split()))
+# numbers = input("Enter integers separated by spaces: ")
+# int_list = list(map(int, numbers.split()))
 
-#Calculating the sum of the integers
-total_sum = sum(int_list)
-print("The sum of all integers is:", total_sum)
+# #Calculating the sum of the integers
+# total_sum = sum(int_list)
+# print("The sum of all integers is:", total_sum)
+
+
+
+# 2.  Program to Create a Tuple of Favorite Books and Print Each Book Name
+#Creating a tuple containing favourite book names
+favourite_books = ("1994", "To Kill a Mockngbird", "Pride and Prejudice", "The Great Gatsby", "Moby-Dick")
+
+#Printing each book name on a separate line
+for book in favourite_books:
+    print(book)
