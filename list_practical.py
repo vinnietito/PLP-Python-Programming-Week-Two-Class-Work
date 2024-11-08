aeb6ab1d4c6d3cd15f581bb593c9691171c2d04a
vinnietito <vincentnjuguna69@gmail.com>
2024-11-08 10:45:42 +0300

    Commits

--- list_practical.py
+++ list_practical.py
@@ -1,5 +1,16 @@
 list_a = [1, 2, 3, 4, 5]
 print(list_a[4])
+print(list_a)
+list_a[0] = 10
+print(list_a)
+
+list_a.insert(len(list_a), 6)
+list_a.append(7)
+list_a.extend([8, 9])
+list_a.pop(8)
+del list_a[7]
+
+print(list_a)
 
 list_b = [1, "today", True, 3.0]
 
