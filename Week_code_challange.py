151007a71083443c1f9ec66017349fe2cd6fc4f8
vinnietito <vincentnjuguna69@gmail.com>
2024-11-08 11:21:24 +0300

    Commits

--- Week_code_challange.py
+++ Week_code_challange.py
@@ -11,8 +11,22 @@
 
 # 2.  Program to Create a Tuple of Favorite Books and Print Each Book Name
 #Creating a tuple containing favourite book names
-favourite_books = ("1994", "To Kill a Mockngbird", "Pride and Prejudice", "The Great Gatsby", "Moby-Dick")
+# favourite_books = ("1994", "To Kill a Mockngbird", "Pride and Prejudice", "The Great Gatsby", "Moby-Dick")
 
-#Printing each book name on a separate line
-for book in favourite_books:
-    print(book)
+# #Printing each book name on a separate line
+# for book in favourite_books:
+#     print(book)
+
+
+
+# 3. Program to Store Information About a Person in a Dictionary
+#Creating a dictionary to store personal information
+person_info = {}
+
+#Asking the user for input
+person_info['name'] = input("Enter your name: ")
+person_info['age'] = int(input("Enter your age: "))
+person_info['favourite_color'] = input("Enter your favourite color: ")
+
+#Printing the dictionary
+print("Person Information: ", person_info)
